c90e5f975f51e789587c4a3103c937b58ab42d5b
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-02-05 08:36:02 -0600

    Improved demo_mr_mash_simple_univ.R.

--- scripts/demo_mr_mash_simple_univ.R
+++ scripts/demo_mr_mash_simple_univ.R
@@ -1,7 +1,9 @@
 # A test that mr_mash_simple also works for univariate linear
 # regression (i.e., r = 1).
+library(mvtnorm)
+library(varbvs)
 source("../code/misc.R")
-source("../code/mr_mash.R")
+source("../code/mr_mash_simple.R")
 
 # SCRIPT PARAMETERS
 # -----------------
@@ -14,7 +16,7 @@ b <- c(-2,1,rep(0,p - 2))
 
 # Variances in the mixture-of-normals prior on the regression
 # coefficients.
-s0 <- list(k1 = 3,k2 = 4,k3 = 6,k4 = 5)
+s0 <- list(k1 = 1e-10,k2 = 4,k3 = 6,k4 = 5)
 
 # The mixture weights in the mixture-of-normals prior on the
 # regression coefficients.
@@ -37,25 +39,22 @@ y <- y - mean(y)
 # -----------------
 # Run 20 co-ordinate ascent updates.
 b0  <- rep(0,p)
-fit <- mr_mash_simple(X,y,1,s0,w0,B0,20)
+fit <- mr_mash_simple(X,y,1,s0,w0,b0,20)
 
 # Compare the posterior mean estimates of the regression coefficients
 # against the coefficients used to simulate the data.
-plot(B,fit$B,pch = 20,xlab = "true",ylab = "estimated")
+plot(b,fit$B,pch = 20,xlab = "true",ylab = "estimated")
 abline(a = 0,b = 1,col = "skyblue",lty = "dotted")
 
-stop()
-
-# Optional: test univariate computations against varbvsmix.
-s0      <- lapply(S0,"[",1)
-s0[[1]] <- 1e-10
-b1      <- mr_mash_simple(Y[,1],X,V[1],s0,w0,B0[,1],20)$B
-
+# FIR VARBVSMIX MODEL
+# -------------------
 s0    <- unlist(s0)
 s0[1] <- 0
-out   <- varbvsmix(X,NULL,Y[,1],V[1]*s0,V[1],w0,matrix(0,p,k),matrix(0,p,k),
-                   update.sigma = FALSE,update.sa = FALSE, update.w = FALSE,
-                   maxiter = 20,tol = 0,drop.threshold = 0,verbose = FALSE)
-b2    <- rowSums(out$alpha * out$mu)
-print(range(b1 - b2)) # Should be close to zero.
+out <- varbvsmix(X,NULL,y,s0,1,w0,matrix(0,p,k),matrix(0,p,k),
+                 update.sigma = FALSE,update.sa = FALSE,update.w = FALSE,
+                 maxiter = 20,tol = 0,drop.threshold = 0,verbose = FALSE)
+b   <- with(out,rowSums(alpha * mu))
+
+# Should be close to zero.
+print(max(abs(fit$B - b)))
 
