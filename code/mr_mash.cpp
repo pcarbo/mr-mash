df4cbad78c7608af927cea68fa081abf6921d601
fmorgante <fmorgante@uchicago.edu>
2020-03-03 14:17:06 -0600

    Add bayes_mvr_ridge_scaled_X function

--- code/mr_mash.cpp
+++ code/mr_mash.cpp
@@ -23,6 +23,11 @@ double bayes_mvr_ridge (const vec& x, const mat& Y, const mat& V,
                         const mat& S0, vec& bhat, mat& S, vec& mu1,
                         mat& S1);
 
+double bayes_mvr_ridge_scaled_X (const vec& b, const mat& S0, const mat& S, const mat& S1, 
+                                 const mat& SplusS0_chol, const mat& S_chol, 
+                                 double ldetSplusS0_chol, double ldet_chol,
+                                 vec& mu1);
+
 double bayes_mvr_mix (const vec& x, const mat& Y, const mat& V,
                       const vec& w0, const cube& S0, vec& mu1, mat& S1,
                       vec& w1);
@@ -68,6 +73,26 @@ List bayes_mvr_ridge_rcpp (const arma::vec& x, const arma::mat& Y,
                       Named("S")     = S,
                       Named("mu1")   = mu1,
                       Named("S1")    = S1,
+                      Named("logbf") = logbf);
+}
+
+// This is mainly used to test the bayes_mvr_ridge_scaled_X C++ function
+// defined below. It is called in the same way as bayes_mvr_ridge_simple,
+// e.g.,
+//
+//    out1 <- bayes_mvr_ridge_simple(x,Y,V,S0)
+//    out2 <- bayes_mvr_ridge_scaled_X_rcpp(b, S0, S, S1, SplusS0_chol, S_chol, ldetSplusS0_chol, ldetS_chol)
+// 
+// [[Rcpp::plugins("cpp11")]]
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::export]]
+List bayes_mvr_ridge_scaled_X_rcpp (const arma::vec& b, const arma::mat& S0, const arma::mat& S, const arma::mat& S1, 
+                                    const arma::mat& SplusS0_chol, const arma::mat& S_chol, 
+                                    double ldetSplusS0_chol, double ldet_chol) {
+  unsigned int r = b.n_elem;
+  vec    mu1(r);
+  double logbf = bayes_mvr_ridge_scaled_X(b, S0, S, S1, SplusS0_chol, S_chol, ldetSplusS0_chol, ldet_chol, mu1);
+  return List::create(Named("mu1")   = mu1,
                       Named("logbf") = logbf);
 }
 
@@ -170,6 +195,22 @@ double bayes_mvr_ridge (const vec& x, const mat& Y, const mat& V,
   return ldmvnorm(bhat,S0 + S) - ldmvnorm(bhat,S);
 }
 
+// Compare this to the R function bayes_mvr_ridge_simple.
+double bayes_mvr_ridge_scaled_X (const vec& b, const mat& S0, const mat& S, const mat& S1, 
+                                 const mat& SplusS0_chol, const mat& S_chol, 
+                                 double ldetSplusS0_chol, double ldetS_chol,
+                                 vec& mu1) {
+  // Compute the posterior mean (mu1) aassuming a multivariate 
+  // normal prior with zero mean and covariance S0.
+  mu1 = S1 * solve(trimatu(S_chol), solve(trimatl(trans(S_chol)), b));
+  
+  // Compute the log-Bayes factor.
+  // return ldmvnorm(bhat,S0 + S) - ldmvnorm(bhat,S);
+  return (ldetS_chol - ldetSplusS0_chol +
+          dot(b, solve(trimatu(S_chol), solve(trimatl(trans(S_chol)), b))) - 
+          dot(b, solve(trimatu(SplusS0_chol), solve(trimatl(trans(SplusS0_chol)), b))))/2;
+}
+
 // Compare this to the R function bayes_mvr_mix_simple.
 double bayes_mvr_mix (const vec& x, const mat& Y, const mat& V,
                       const vec& w0, const cube& S0, vec& mu1, mat& S1,
@@ -229,3 +270,13 @@ double ldmvnorm (const arma::vec& x, const arma::mat& S) {
   double d = norm(solve(L,x),2);
   return -(d*d)/2 - sum(log(L.diag()));
 }
+
+
+
+
+
+
+
+
+
+
